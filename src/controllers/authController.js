a087c9b2de2c1d12075d358c286628c98988d413
datdev2409 <datdev2409@gmail.com>
2024-02-20 21:02:31 +0700

    feat: change auth response

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -10,39 +10,32 @@ function generateToken(user) {
   return token;
 }
 
+const sendAuthResponse = (res, user) => {
+  return res.status(200).json({
+    status: "success",
+    data: {
+      access_token: generateToken(user),
+      user: _.pick(user, ["name", "phone", "avatar"]),
+    },
+  });
+};
+
 const authController = {
   signUp: async (req, res) => {
     const user = await User.create(req.body);
 
-    res.status(201).json({
-      status: "success",
-      data: {
-        access_token: generateToken(user),
-        user: user,
-      },
-    });
+    sendAuthResponse(res, user);
   },
 
   login: async (req, res) => {
     const { phone, password } = req.body;
-
-    if (!phone || !password) {
-      throw new AppError("Missing fields", 400);
-    }
-
     const user = await User.findOne({ phone }).select("+password");
 
     if (!user || !user.comparePassword(password)) {
       throw new AppError("Incorect phone or password", 400);
     }
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        access_token: generateToken(user),
-        user: _.pick(user, ["name", "email", "phone"]),
-      },
-    });
+    sendAuthResponse(res, user);
   },
 };
 
