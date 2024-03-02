8b5d76e075d40bb4e057a95457bd6a4f6e997f93
vide_y_sora <nguyenphuoc2508.lhp.bk@gmail.com>
2024-03-02 15:08:08 +0700

    refactor: refactor logic of fucntion getRadius

--- src/utils/getRadius.js
+++ src/utils/getRadius.js
@@ -1,17 +1,11 @@
 const getRadius = (distance, unit) => {
-  let divider
-  switch (unit) {
-    case "mi":
-      divider = 3963.2;
-      break;
-    case "km":
-      divider = 6378.1;
-      break;
-    case "m":
-      divider = 6378100;
-      break;
+  const dividerMapping = {
+    km: 6378.1,
+    mi: 3963.2,
+    m: 6378100,
   }
-  return distance / divider;
+
+  return distance / dividerMapping[unit];
 }
 
 module.exports = getRadius;
